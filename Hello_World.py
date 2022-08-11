c6d650f276ecb0c8d829f02b0ad60eab22c27ee5
Abdelrahman Wael Abdelrahman Rashad Ammar <73370043+bodiwael@users.noreply.github.com>
2022-08-11 20:28:38 +0200

    Add files via upload

---
+++ Hello_World.py
@@ -0,0 +1,15 @@
+# How could print a Text
+
+print ("Hello World !") # Simply type (print)
+
+''' 
+Any Questions:
+    
+    Eng. Abdelrahman Wael Ammar
+    
+    Phone: +201010331884
+    
+    E-Mail: bodiwael999@gmail.com
+
+
+'''
